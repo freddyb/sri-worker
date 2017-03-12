12e2fa7a1319ea25efaba8ec3c0eff703f38944e
Frederik B <freddy@flashpad>
2017-03-12 12:38:55 +0100

    linting failures, nicer logging etc.

--- sri-worker.js
+++ sri-worker.js
@@ -1,24 +1,23 @@
-var cacheName = 'sri-worker-0.0.1';
-
 const INTEGRITY_METADATA = {
   // this could live in its own separate file.
   // but preferrably inlined for performance.
-  'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js': 'sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa',
-  'https://html5sec.org/test.js': 'sha384-moo',
-  'http://code.jquery.com/jquery-3.1.1.min.js': 'sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8='
-}
-const INTEGRITY_LEVEL = 0; // >9000 means require-sri-for.
+  "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js": "sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa",
+  "https://html5sec.org/test.js": "sha384-moo",
+  "http://code.jquery.com/jquery-3.1.1.min.js": "sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+};
+const INTEGRITY_LEVEL = 0; // >9000 means require-sri-for. mo
 
-self.addEventListener('install', function(evt) {
+const LOGNAME = "SRI-ServiceWorker";
+self.addEventListener("install", function() {
     return self.skipWaiting();
 });
-self.addEventListener('activate', function (evt) {
-  evt.waitUntil(self.clients.claim());
+self.addEventListener("activate", function(event) {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function(event) {
   let request = event.request;
-  if ((request.method == 'GET') &&
+  if ((request.method === "GET") &&
      (request.url in INTEGRITY_METADATA) &&
      (!request.integrity)) {
     // can fetch with SRI
@@ -26,11 +25,11 @@ self.addEventListener('fetch', function(event) {
     let init = {
       integrity: sriHash,
       method: request.method,
-      mode: 'cors', // if we have integrity metadata, it should also use cors
-      credentials: 'omit', // ...and omit credentials
-      cache: 'default',
-    }
-    console.info('[ServiceWorker] Adding integrity metadata for fetch', request.url, '('+sriHash+')');
+      mode: "cors", // if we have integrity metadata, it should also use cors
+      credentials: "omit", // ...and omit credentials
+      cache: "default",
+    };
+    console.info(`[${LOGNAME}] Adding integrity metadata for fetch ${request.url}, (${sriHash})`);
     event.respondWith(fetch(request, init));
   } else if (INTEGRITY_LEVEL > 9000) {
     // INTEGRITY_LEVEL OVER 9000 means SRI strictly required.
@@ -38,11 +37,11 @@ self.addEventListener('fetch', function(event) {
        integrity of *all* your scripts, then it should happen in the platform,
         not in JavaScript
     */
-    console.error('[ServiceWorker] Can not fetch', request.url+".", "No integrity metadata.");
-    event.respondWith(Response.error())
+    console.error(`[${LOGNAME}] Can not fetch ${request.url}. No integrity metadata.`);
+    event.respondWith(Response.error());
   } else {
     // else, fetch as is.
-    console.warn('[ServiceWorker] Can not add integrity metadata for fetch', request.url);
+    console.warn(`[${LOGNAME}] Can not add integrity metadata to fetch ${request.url}`);
     event.respondWith(fetch(request));
   }
 });
